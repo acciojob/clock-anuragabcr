e4d5c34d15056ab3d38b112ed19a60eef97cc9b1
Anurag Kumar <47923173+anuragabcr@users.noreply.github.com>
2024-05-14 15:12:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+let timer = document.getElementById("timer")
+
+let time = new Date()
+
+setInterval(
+	function () {
+		time = new Date()
+		timer.innerHTML = time.toLocaleString()
+	}, 1000
+)
